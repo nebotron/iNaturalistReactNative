fbd5b6ac026d14a7b79911608618adf9c2714e02
Ben Hannel <bfhannel@gmail.com>
2026-05-29 06:45:34 -0700

    Fix tests, strings

--- src/sharedHelpers/shouldPromptDeleteOriginalPhotos.ts
+++
@@ -1,7 +0,0 @@
-import useStore from "stores/useStore";
-
-const shouldPromptDeleteOriginalPhotos = ( ): boolean => (
-  useStore.getState( ).originalDevicePhotoUris.length > 0
-);
-
-export default shouldPromptDeleteOriginalPhotos;
